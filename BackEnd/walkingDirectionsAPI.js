74dff9e9727685681ab80267c8583c0accd03831
Luke Butcher <butcher_l@icloud.com>
2026-01-09 13:33:42 -0500

    Add OpenRoutingService API for walking directions - API Backend

---
+++ BackEnd/walkingDirectionsAPI.js
@@ -0,0 +1,51 @@
+import polyline from 'polyline';
+
+// Coordinates should be in [longitude, latitude] format
+export async function getWalkingDirections(startCoordsFirst, endCoordsFirst, startCoordsSecond, endCoordsSecond) {
+    const firstDirections = fetchDirections(startCoordsFirst, endCoordsFirst);
+    const secondDirections = fetchDirections(startCoordsSecond, endCoordsSecond);
+    return Promise.all([firstDirections, secondDirections]);
+}
+
+async function fetchDirections(startCoords, endCoords) {
+    const OSREndpoint = "https://api.openrouteservice.org/v2/directions/foot-walking/json";
+    try {
+        const response = await fetch(OSREndpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+                "Authorization": process.env.OPENROUTESERVICE_API_KEY || ""
+            },
+            body: JSON.stringify({coordinates: [startCoords, endCoords]})
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        const route = result.routes[0];
+        const segments = route.segments[0];
+
+        return {
+            polyline: polyline.decode(route.geometry).map(([lat, lng]) => ({
+                latitude: lat,
+                longitude: lng
+            })),
+            distance: segments.distance,
+            duration: segments.duration,
+            steps: segments.steps.map((step) => ({
+                distance: step.distance,
+                duration: step.duration,
+                instruction: step.instruction,
+                type: step.type,
+                name: step.name,
+                wayPoints: step.way_points
+            }))
+        };
+
+    } catch (error) {
+        console.error(error.message);
+        throw error;
+    }
+}
